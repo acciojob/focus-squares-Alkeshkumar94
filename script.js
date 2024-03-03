5d9931616d622b3464bfaf9a95ce316593d9933d
Alkesh kumar <127075190+Alkeshkumar94@users.noreply.github.com>
2024-03-03 17:35:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+ const squares = document.querySelectorAll('.square');
+
+  squares.forEach(square => {
+    square.addEventListener('mouseenter', () => {
+      squares.forEach(s => {
+        if (s !== square) {
+          s.style.backgroundColor = '#6F4E37'; // Change color to Coffee
+        }
+      });
+    });
+
+    square.addEventListener('mouseleave', () => {
+      squares.forEach(s => {
+		  if(s!== square){
+        s.style.backgroundColor = '#E6E6FA'; // Reset color to Lavender
+		  }
+      });
+    });
+  });
